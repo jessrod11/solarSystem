89a3108196be3e59ef75da030d222537c9b8b5b1
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-03-28 20:13:00 -0500

    called data with xhr request and printed names to the page

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
 console.log('Stix');
+
+const printToDom = (domString, divId) => {
+    document.getElementById(divId).innerHTML = domString;
+};
+
+const buildDomString = (planetArray) => {
+    let domString = '';
+    planetArray.forEach((planets)=>{
+        domString += `<div id="card-container">`;
+        domString +=    `<h1>${planets.name}</h1>`;
+        domString += `</div>`;
+    })
+    printToDom(domString, 'planet-container');
+};
+
+function executeWhenCodeFails() {
+    console.log('What happened, Stix?!');
+};
+
+function executeWhenPageLoads (){
+    const data = JSON.parse(this.responseText);
+    buildDomString(data.planets);
+};
+
+
+const startApplication = () => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener('load', executeWhenPageLoads);
+    myRequest.addEventListener('error', executeWhenCodeFails);
+    myRequest.open('GET', 'planets.json');
+    myRequest.send();
+};
+
+startApplication();
